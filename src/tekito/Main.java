a7c8db3ceccb43a9b23c7f0b3b3f866c28b4e37b
牧原慈仁 <makiharajihitoshi@ip-10-95-3-240.ap-northeast-1.compute.internal>
2025-05-13 14:27:21 +0900

    first commit

---
+++ src/tekito/Main.java
@@ -0,0 +1,22 @@
+package tekito;
+
+public class Main {
+
+	public static void showTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // 改行
+        }
+    }
+
+    // メインメソッド（動作確認用）
+    public static void main(String[] args) {
+        showTriangle(5); // 例: 5段の三角形を表示
+    }
+		
+	}
+	
+
+
